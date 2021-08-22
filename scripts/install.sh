cc8c5603c50200b1c4f078a3ea7e38463fa187d0
Tanja Polz <tp@simplydelivery.de>
2021-08-22 18:27:35 +0200

    - add nodes to keep every command in mind and documented - add Vagrant for installing terraform in a vm - install.sh for starting vm and installing terraform

---
+++ scripts/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# remove comment if you want to enable debugging
+#set -x
+
+if [ -e /etc/redhat-release ] ; then
+  REDHAT_BASED=true
+fi
+
+TERRAFORM_VERSION="1.0.5"
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/mykey ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
+# install packages
+if [ ${REDHAT_BASED} ] ; then
+  yum -y update
+else 
+  apt-get update
+fi
+
+
+#terraform
+T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+
+# clean up
+if [ ! ${REDHAT_BASED} ] ; then
+  apt-get clean
+fi
