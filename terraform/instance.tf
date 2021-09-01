cfcbdf34616bc2f5d58d593293bc1b702ed73d17
Tanja Polz <tp@simplydelivery.de>
2021-09-01 22:50:14 +0200

    updated EC2 creation so it's connected to subnet

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,9 +1,4 @@
 
-resource "aws_instance" "devops" {
-  ami           = var.AMIS[var.AWS_REGION]
-  instance_type = var.INSTANCE_TYPE
-}
-
 # Create a VPC
 resource "aws_vpc" "DevOps-VPC"{
     cidr_block = var.CIDR_BLOCK[0]
@@ -11,7 +6,6 @@ resource "aws_vpc" "DevOps-VPC"{
     tags = {
         Name = "DevOps-VPC"
     }
-
 }
 
 # Create Subnet
@@ -33,7 +27,6 @@ resource "aws_internet_gateway" "DevOps-IntGW" {
     }
 }
 
-
 # Create Secutity Group (acts like virtuel firewall to controll inbound and outbound traffic)
 resource "aws_security_group" "DevOps_Sec_Group" {
   name = "DevOps Security Group"
@@ -41,7 +34,6 @@ resource "aws_security_group" "DevOps_Sec_Group" {
   vpc_id = aws_vpc.DevOps-VPC.id
 
 # specify the inbound traffic
-
 # allow dynamic incoming ports 
   dynamic ingress {
       iterator = port
@@ -68,7 +60,6 @@ resource "aws_security_group" "DevOps_Sec_Group" {
   }
 }
 
-
 # Create route table and association
 resource "aws_route_table" "DevOps_RouteTable" {
     vpc_id = aws_vpc.DevOps-VPC.id
@@ -89,3 +80,19 @@ resource "aws_route_table_association" "DevOps_Assn" {
     subnet_id = aws_subnet.DevOps-Subnet1.id
     route_table_id = aws_route_table.DevOps_RouteTable.id
 }
+
+# Create an AWS EC2 Instance (allows instances to be created, updated and deleted. Instancec also support provisioning)
+# AMI = Amazon machine imgag -> template that contains software configs (operating system, application etc.)
+resource "aws_instance" "devops" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = var.INSTANCE_TYPE
+  key_name = "EC2"
+  vpc_security_group_ids = [aws_security_group.DevOps_Sec_Group.id]
+  subnet_id = aws_subnet.DevOps-Subnet1.id
+  associate_public_ip_address = true
+  
+  tags = {
+    Name = "devops"
+  }
+}
+
