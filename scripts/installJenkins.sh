7e71f6f09a7b0ba9b74d8ccc2f110140b42f928f
Tanja Polz <tp@simplydelivery.de>
2021-09-04 19:29:12 +0200

    because previous code was executed only about every 8 times, template created for user data

--- scripts/installJenkins.sh
+++ scripts/installJenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+
 #Jenkins Installation:
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk --yes
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-echo "deb https://pkg.jenkins.io/debian binary/" >> /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y jenkins
-systemctl status jenkins
-find /usr/lib/jvm/java-1.8* | head -n 3
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install openjdk-8-jdk -y
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+
--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,3 +1,9 @@
+# userdata for the Jenkins server ...
+data "template_file" "jenkins_server" {
+  template = "${file("../scripts/installJenkins.sh")}"
+
+}
+
 # Create an AWS EC2 Instance (allows instances to be created, updated and deleted. Instancec also support provisioning)
 # AMI = Amazon machine imgag -> template that contains software configs (operating system, application etc.)
 resource "aws_instance" "devops" {
@@ -7,17 +13,25 @@ resource "aws_instance" "devops" {
   vpc_security_group_ids = [aws_security_group.DevOps_Sec_Group.id]
   subnet_id = aws_subnet.DevOps-Subnet1.id
   associate_public_ip_address = true
-  user_data = file("../scripts/installJenkins.sh")
+  user_data = "${data.template_file.jenkins_server.rendered}"
   
   tags = {
     Name = "devops"
   }
-
-  # connection {
-  #   host        = coalesce(self.public_ip, self.private_ip)
-  #   type        = "ssh"
-  #   user        = var.INSTANCE_USERNAME
-  #   private_key = file(var.PATH_TO_PRIVATE_KEY)
-  # }
 }
+
+# # Create an AWS EC" Instance to host Ansible Controller (control node)
+# resource "aws_instance" "AnsibleController" {
+#   ami           = var.AMIS[var.AWS_REGION]
+#   instance_type = var.INSTANCE_TYPE
+#   key_name      = aws_key_pair.mykey.key_name
+#   vpc_security_group_ids = [aws_security_group.DevOps_Sec_Group.id]
+#   subnet_id = aws_subnet.DevOps-Subnet1.id
+#   associate_public_ip_address = true
+#   user_data = file("../scripts/installAnsibleCN.sh")
+  
+#   tags = {
+#     Name = "Ansible-ConrolNode"
+#   }
+# }
 
