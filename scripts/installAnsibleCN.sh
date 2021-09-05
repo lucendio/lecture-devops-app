219756a6adf4d4dfda31ed6c923c1d677c6aee29
Tanja Polz <tp@simplydelivery.de>
2021-09-05 15:42:29 +0200

    create sudo enabled user on ansible controller instance

--- scripts/installAnsibleCN.sh
+++ scripts/installAnsibleCN.sh
@@ -18,4 +18,13 @@ if ! [ -x "$(command -v ansible)" ]; then
 
 fi
 
-
+sudo su -
+sudo useradd ansibleadmin
+sudo echo "ansibleadmin:ansible" | chpasswd
+# modify the sudoers file at /etc/sudoers and add entry
+echo 'ansibl_eadmin     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+echo 'ubuntu     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
+# this command is to add an entry to file : echo 'PasswordAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
+# the below sed command will find and replace words with spaces "PasswordAuthentication no" to "PasswordAuthentication yes"
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo service sshd restart
