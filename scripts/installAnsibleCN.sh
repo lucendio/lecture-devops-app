5a50d9f61bc1439aefed04361686c5b2047fe7bd
Tanja Polz <tp@simplydelivery.de>
2021-09-05 19:46:15 +0200

    generate ssh-key with script, manuelly endet up in permission denied

--- scripts/installAnsibleCN.sh
+++ scripts/installAnsibleCN.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-# install ansible
+
 #!/bin/bash
 set -e
 
+# install ansible
 if ! [ -x "$(command -v apt-add-repository)" ]; then 
     sudo apt-get --assume-yes update 
     sudo apt-get install --assume-yes software-properties-common
@@ -27,4 +28,12 @@ echo 'ubuntu     ALL=(ALL)      NOPASSWD: ALL' | sudo tee -a /etc/sudoers
 # this command is to add an entry to file : echo 'PasswordAuthentication yes' | sudo tee -a /etc/ssh/sshd_config
 # the below sed command will find and replace words with spaces "PasswordAuthentication no" to "PasswordAuthentication yes"
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo adduser ansibleadmin sudo
+# create new ssh key
+[[ ! -f /home/ansibleadmin/.ssh/id_rsa ]] \
+&& mkdir -p /home/ansibleadmin/.ssh \
+&& ssh-keygen -f /home/ansibleadmin/.ssh/id_rsa -N '' \
+&& chown -R ansibleadmin:ansibleadmin /home/ansibleadmin/.ssh
 sudo service sshd restart
+
+
