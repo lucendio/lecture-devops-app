253d45b0c5c983809da3633675b590be9a2da559
Tanja Polz <tp@simplydelivery.de>
2021-09-12 15:24:03 +0200

    added cause sonarqube doesn't work without

--- scripts/installSonar.sh
+++ scripts/installSonar.sh
@@ -1,22 +1,16 @@
 #! /bin/bash
 
-# sudo apt-get update -y
-# sudo apt-get upgrade -y
-# sudo add-apt-repository ppa:webupd8team/java
-# sudo apt-get update -y
-# sudo apt-get install oracle-java8-installer -y
-# sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-# wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
-# sudo apt-get update -y
-# sudo apt-get install postgresql postgresql-contrib
-# su - postgres
-# createuser sonar
-# psql
-# ALTER USER sonar WITH ENCRYPTED password 'sonar';
-# CREATE DATABASE sonar OWNER sonar;
-# \q
-# sudo adduser sonar
-# wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.6.zip
-# unzip sonarqube-6.7.6.zip
-# sudo cp -r sonarqube-6.7.6 /opt/sonarqube
-# sudo chown -R sonar:sonar /opt/sonarqube
+#! /bin/bash
+
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+cd /opt/
+sudo wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+sudo tar xf latest-unix.tar.gz
+sudo mv nexus-3.* nexus3
+cd /opt/nexus3/bin/
+sudo touch nexus.rc
+sudo ln -s /opt/nexus3/bin/nexus /etc/init.d/nexus
+cd /etc/init.d
+sudo systemctl enable nexus
+sudo systemctl start nexus
